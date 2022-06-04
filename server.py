d886f0378060a32e9dce7e5588273edabc3b3c2f
wawachuchu <60635447+gtopacio@users.noreply.github.com>
2022-06-04 16:47:12 +0800

    Initial Server

--- server.py
+++ server.py
@@ -2,17 +2,43 @@ import os
 import boto3
 import json
 import firebase_admin
-from firebase_admin import firestore 
+from firebase_admin import firestore, storage
 import signal
+import sys
+from server_utils import clean_folder, generateURL
 
-SQS_QUEUE_URL = os.getenv('SQS_QUEUE_URL')
+os.environ["AWS_ACCESS_KEY_ID"] = "AKIAQX7GHQCG6HJXNNND"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "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"
+os.environ["SQS_QUEUE_URL"] = "https://sqs.ap-southeast-1.amazonaws.com/051485573261/digimap.fifo"
+
+# os.environ["AWS_ACCESS_KEY_ID"] = "AKIAQX7GHQCG6BC5KUUQ"
+# os.environ["AWS_SECRET_ACCESS_KEY"] = "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"
+# os.environ["SQS_QUEUE_URL"] = "https://sqs.ap-southeast-1.amazonaws.com/051485573261/digimap-dlq.fifo"
+
+os.environ["FIREBASE_BUCKET"] = "digimap-3dp.appspot.com"
+
+SQS_QUEUE_URL = os.getenv("SQS_QUEUE_URL")
+FIREBASE_BUCKET = os.getenv("FIREBASE_BUCKET")
+
+BOOST_BASE = 'BoostingMonocularDepth'
+BOOST_INPUTS = 'inputs'
+BOOST_OUTPUTS = 'outputs'
 
 if __name__ == "__main__":
 
+    os.makedirs("image", exist_ok=True)
+
     running = True
-    waitTime = 20
-    sqs = s3 = boto3.resource('sqs')
-    waitTimeCap = 500
+    waitTime = 1
+    waitTimeCap = 20
+    visibilityTimeout = 2
+
+    cred_obj = firebase_admin.credentials.Certificate("firebase-admin-key.json")
+    default_app = firebase_admin.initialize_app(cred_obj, options=None, name="FirestoreDB")
+    firestore = firestore.client(app=default_app)
+    bucket = storage.bucket(name=FIREBASE_BUCKET, app=default_app)
+
+    sqs = boto3.client("sqs", region_name="ap-southeast-1")
 
     def signal_handler(sig, frame):
         global running
@@ -22,33 +48,72 @@ if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
 
     while running:
+
         response = sqs.receive_message(
-        QueueUrl=SQS_QUEUE_URL,
-        MaxNumberOfMessages=1,
-        WaitTimeSeconds=waitTime,
+            QueueUrl=SQS_QUEUE_URL,
+            AttributeNames=[
+                'SentTimestamp'
+            ],
+            MaxNumberOfMessages=1,
+            MessageAttributeNames=[
+                'All'
+            ],
+            VisibilityTimeout=visibilityTimeout,
+            WaitTimeSeconds=waitTime
         )
 
         if(len(response.get('Messages', [])) <= 0):
+            waitTime *= 2
             if waitTime > waitTimeCap:
-                waitTime = 20
-            else:
-                waitTime *= 2
+                waitTime = 1
             continue
 
         for message in response.get("Messages", []):
+            
+            try:
+                message_body = message["MessageAttributes"]
+                receiptHandle = message['ReceiptHandle']
+                id = message_body["id"]["StringValue"]
+                traj = message_body["traj"]["StringValue"]
+                config = os.path.join("arguments", traj+".yml")
 
-            message_body = message["Body"]
-            receiptHandle = message['ReceiptHandle']
-            messageBody = json.loads(message_body)
-            print(messageBody.id)
+                firestore.document(f"jobs/{id}").update({u'status': "PROCESSING"})
+                imageBlob = bucket.blob(id+".jpg")
+                imageBlob.download_to_filename(os.path.join("image", id+".jpg"))
 
-    # Get SQS
-    # Download Image
-    # Change Firebase State to PROCESSING
-    # Run 3DP
-    # Change Firebase State to UPLOADING
-    # Upload video
-    # Get Links
-    # Set Links to Firebase
-    # Remove Message from SQS
-    # Delete Residual Files
+                os.system(f'python main.py --config {config}')
+
+                firestore.document(f"jobs/{id}").update({u'status': "UPLOADING"})
+                videoFileName = id + '_' + traj + '.mp4'
+                videoBlob = bucket.blob(videoFileName)
+                videoBlob.upload_from_filename(os.path.join("video", videoFileName))
+
+                signedURL = generateURL(videoFileName)
+                firestore.document(f"jobs/{id}").update({
+                    u'status': "UPLOADED",
+                    u'link': signedURL
+                })
+
+                imageBlob.delete()
+                sqs.delete_message(
+                    QueueUrl=SQS_QUEUE_URL,
+                    ReceiptHandle=receiptHandle
+                )
+
+            except Exception as e:
+                firestore.document(f"jobs/{id}").update({
+                    u'status': "FAILED",
+                    u'message': str(e)
+                })
+                print(e)
+
+            finally:
+                clean_folder(os.path.join(BOOST_BASE, BOOST_INPUTS))
+                clean_folder(os.path.join(BOOST_BASE, BOOST_OUTPUTS))
+                clean_folder(os.path.join("image"))
+                clean_folder(os.path.join("depth"))
+                clean_folder(os.path.join("mesh"))
+                clean_folder(os.path.join("video"))
+
+        waitTime = 1
+        sys.exit(0)
---
+++ server_utils.py
@@ -0,0 +1,24 @@
+import os
+import glob
+import datetime
+
+def clean_folder(folder, img_exts=['.png', '.jpg', '.npy', '.mp4']):
+
+    for img_ext in img_exts:
+        paths_to_check = os.path.join(folder, f'*{img_ext}')
+        if len(glob.glob(paths_to_check)) == 0:
+            continue
+        print(paths_to_check)
+        if(os.name is "nt"):
+            os.system(f'rd {paths_to_check}')
+        else:
+            os.system(f'rm {paths_to_check}')
+
+def generateURL(bucket, blob_name):
+    blob = bucket.blob(blob_name)
+    url = blob.generate_signed_url(
+        version="v4",
+        expiration=datetime.timedelta(minutes=15),
+        method="GET",
+    )
+    return url
